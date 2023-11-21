b67a887496f815bffef561538afc0083a44e54cd
codenaud <codenaud@gmail.com>
2023-11-21 18:40:59 +0100

    Add block 1.4. New nav and css styles

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
-/* const userName = prompt('Antes de empezar, me gustaría saber tu nombre. ¿Cómo te llamas?');
-console.log(`Hola, ${userName} bienvenid@!`); */
+// Styles for console.log
+// 1. Pass the css styles in an array
+const styleLog = [
+  'color: #333',
+  'background: #ffd500',
+  'font-size: .8rem',
+  'padding: 2px 5px',
+  'border-radius:5px',
+].join(';');
+
+/* // descomentar para personalizar el nombre ↓↓↓
+ const userName = prompt('Antes de empezar, me gustaría saber tu nombre. ¿Cómo te llamas?');
+console.log(`%cHola, ${userName} bienvenid@!`, styleLog); */
+
+// Empezamos
 console.log('A continuación te presento las soluciones del Sprint 2 de Angular');
 console.log('----------------------------------------------');
 console.log('### Arrow functions');
 console.log('----------------------------------------------');
-console.log('► Nivel 1');
-console.log('►► Ejercicio 1');
+console.log('## Nivel 1 [AF]');
+console.log('# Ejercicio 1');
 
 /*  -- Ejercicio 1:
     -- Ejercicio: convierte esta función en una función de flecha */
@@ -23,7 +36,8 @@ console.log(sum);
 
 // -------------------------------------------------------------------
 
-console.log('►► Ejercicio 2');
+console.log('----------------------------------------------');
+console.log('# Ejercicio 2');
 
 /*  -- Ejercicio 2:
     -- Función flecha número random de 0,100 */
@@ -34,7 +48,8 @@ console.log(myRandomNumber);
 
 // -------------------------------------------------------------------
 
-console.log('►► Ejercicio 3');
+console.log('----------------------------------------------');
+console.log('# Ejercicio 3');
 
 /*  -- Ejercicio 3:
     -- Crea la clase persona + saludar a persona
@@ -44,14 +59,14 @@ greet('Ismael');
 // -------------------------------------------------------------------
 
 console.log('----------------------------------------------');
-console.log('► Nivel 2');
-console.log('►► Ejercicio 4');
+console.log('## Nivel 2 [AF]');
+console.log('# Ejercicio 4');
 
 /*  -- Ejercicio 4 :
     -- Función flecha dentro de un loop 'printNumbers
     -- numbers = [10,20,30,40,50,60,70,80,90,100] */
 
-console.log('►►► Array utilizada => [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]');
+console.log('# ► Array utilizada => [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]');
 
 const numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
@@ -65,27 +80,29 @@ printNumbers(numbers);
 // -------------------------------------------------------------------
 
 console.log('----------------------------------------------');
-console.log('► Nivel 3');
-console.log('►► Ejercicio 5');
+console.log('## Nivel 3 [AF]');
+console.log('# Ejercicio 5');
 /*  -- Ejercicio 5 :
     -- Función flecha con 'setTimeout'
     -- que imprima el mensaje 'Acaban de pasar 3 segundos' en consola */
 
 setTimeout(() => {
-  console.log('AF => Nivel 3 => Ejercicio 5: Buah! 5 segundos... Locura.');
+  console.log('⏱ AF => Nivel 3 => Ejercicio 5: Buah! 5 segundos... Locura.');
 }, 5000);
 setTimeout(() => {
-  console.log('AF => Nivel 3 => Ejercicio 5: Ahora acaban de pasar 3 segundos');
+  console.log('⏱ AF => Nivel 3 => Ejercicio 5: Ahora acaban de pasar 3 segundos');
 }, 3000);
 setTimeout(() => {
-  console.log('AF => Nivel 3 => Ejercicio 5: Acaba de pasar 1 segundos');
+  console.log('⏱ AF => Nivel 3 => Ejercicio 5: Acaba de pasar 1 segundos');
 }, 1000);
+
+// -------------------------------------------------------------------
 
 console.log('----------------------------------------------');
 console.log('### Operador Ternario');
 console.log('----------------------------------------------');
-console.log('► Nivel 1');
-console.log('►► Ejercicio 1 [Introduce la edad en el DOM]');
+console.log('## Nivel 1 [OT]');
+console.log('# Ejercicio 1 [Introduce la edad en el DOM]');
 
 /*  -- Ejercicio 1:
     -- Operador ternario básico 
@@ -100,7 +117,9 @@ const potConduir = () => {
 
 // -------------------------------------------------------------------
 
-console.log('►► Ejercicio 2 [Introduce los números en el DOM]');
+console.log('----------------------------------------------');
+
+console.log('# Ejercicio 2 [Introduce los números en el DOM]');
 
 /*  -- Ejercicio 2:
     -- Operador ternario de compración  
@@ -118,8 +137,8 @@ const compararNum = () => {
 // -------------------------------------------------------------------
 
 console.log('----------------------------------------------');
-console.log('► Nivel 2');
-console.log('►► Ejercicio 3 [Introduce los valores a determinar en el DOM]');
+console.log('## Nivel 2 [OT]');
+console.log('# Ejercicio 3 [Introduce los valores a determinar en el DOM]');
 
 /*  -- Ejercicio 3:
     -- Operador ternario enlazado  */
@@ -135,7 +154,9 @@ const determinarNum = () => {
 
 // -------------------------------------------------------------------
 
-console.log('►► Ejercicio 3.b [Introduce 3 valores en el DOM]');
+console.log('----------------------------------------------');
+
+console.log('# Ejercicio 3.b [Introduce 3 valores en el DOM]');
 
 /*  -- Ejercicio 3.b:
     -- Operador ternario enlazado
@@ -160,13 +181,13 @@ const maxNum = () => {
 // -------------------------------------------------------------------
 
 console.log('----------------------------------------------');
-console.log('► Nivel 3');
-console.log('►► Ejercicio 4');
+console.log('## Nivel 3 [OT]');
+console.log('# Ejercicio 4');
 
 /*  -- Ejercicio 4 :
     -- parOImparArray = [1,2,3,4,5,6,7,8,9,10] */
 
-console.log('►►► Array utilizada => => [1,2,3,4,5,6,7,8,9,10]');
+console.log('# ► Array utilizada => [1,2,3,4,5,6,7,8,9,10]');
 
 const parOImparArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -178,23 +199,121 @@ const parOImpar = (parOImparArray) => {
 };
 parOImpar(parOImparArray);
 
+// -------------------------------------------------------------------
+
 console.log('----------------------------------------------');
 console.log('### Callbacks');
 console.log('----------------------------------------------');
-console.log('► Nivel 1');
-console.log('►► Ejercicio 1 [Introduce la edad en el DOM]');
+console.log('## Nivel 1 [CB]');
+console.log('# Ejercicio 1');
 
 /*  -- Ejercicio 1:
     -- Callback básico 
     -- función: procesar invoca función de callback */
 
-/* function processar(num, callback) {
-  // hacemo algo ...
-  num = 10;
-  // despues de hacer algo ...
-  callback(num);
-}
+// Definición de la función procesar
+const procesar = (numero, callback) => {
+  // Invocar la función de callback y pasar el número como parámetro
+  callback(numero);
+};
 
-processar(nums, function (num) {
-  console.log(`He añadido el ${num} como parámetro`);
-}); */
+// Ejemplo de uso
+const miCallback = (numero) => {
+  console.log(`El número procesado es: ${numero}`);
+};
+
+// Llamada a la función procesar, pasando un número y el callback
+procesar(42, miCallback);
+
+// -------------------------------------------------------------------
+
+console.log('----------------------------------------------');
+
+console.log('# Ejercicio 2');
+
+/* // descomentar para personalizar el nombre ↓↓↓
+ console.log(`%cSeguimos! ${userName}`, styleLog); */
+
+/*  -- Ejercicio 2:
+    -- Callback en operaciones matemáticas
+    -- función: calculadora */
+
+const calculadora = (num1, num2, callback) => {
+  callback(num1, num2);
+};
+
+// Calculadora
+const sumaCallback = (num1, num2) => {
+  let resultado = num1 + num2;
+  console.log(`La suma de ${num1} + ${num2} es ${resultado}`);
+};
+
+// Llamada a la función procesar, pasando un número y el callback
+calculadora(5, 3, sumaCallback);
+
+// -------------------------------------------------------------------
+
+console.log('----------------------------------------------');
+console.log('## Nivel 2 [CB]');
+console.log('# Ejercicio 3');
+
+/*  -- Ejercicio 3 :
+    -- callback en funciones asíncronas 
+    -- Función esperarISaludar */
+
+const esperarISaludar = (name1, callback) => {
+  setTimeout(() => {
+    callback(name1);
+  }, 2000);
+};
+
+const saludaCallback = (name1) => {
+  console.log(`⏳ CB => Nivel 2 => Ejercicio 3: Hello! ${name1}`);
+};
+// Llamada a la función esperarISaludar, pasando el nombre1 y el callback
+esperarISaludar('Ismael', saludaCallback);
+
+// -------------------------------------------------------------------
+
+console.log('----------------------------------------------');
+
+console.log('# Ejercicio 4');
+
+/*  -- Ejercicio 4 :
+    -- callback con arrays
+    -- Función processarElements */
+
+const processarElements = (miArray, callback) => {
+  for (let i = 0; i < miArray.length; i++) {
+    callback(miArray[i]);
+  }
+};
+
+// Ejemplo de uso
+const myCallback = (elemento) => {
+  console.log(`Procesando elemento: ${elemento}`);
+};
+
+const arrayEjemplo = [1, 2, 3, 4, 5];
+processarElements(arrayEjemplo, miCallback);
+
+// -------------------------------------------------------------------
+
+console.log('----------------------------------------------');
+console.log('## Nivel 3 [CB]');
+console.log('# Ejercicio 5');
+
+/*  -- Ejercicio 5 :
+    -- Función processarCadena */
+
+const processarCadena = (miCadena, callback) => {
+  const cadenaEnMayusculas = miCadena.toUpperCase();
+  callback(cadenaEnMayusculas);
+};
+
+// Ejemplo de uso
+const cadenaCallback = (cadenaEnMayusculas) => {
+  console.log(`Aquí tienes tu cadena: \n${miCadena} \nen mayúsculas \n${cadenaEnMayusculas}`);
+};
+let miCadena = 'como mola it academy';
+processarCadena(miCadena, cadenaCallback);
