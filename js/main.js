26dff12986ccb810b8d56db950bde7357332a57b
Codenaud <codenaud@gmail.com>
2023-11-21 13:30:27 +0100

    Finishing level 2. Starting level 3

--- js/main.js
+++ js/main.js
@@ -46,9 +46,12 @@ greet('Ismael');
 console.log('----------------------------------------------');
 console.log('► Nivel 2');
 console.log('►► Ejercicio 4');
+
 /*  -- Ejercicio 4 :
     -- Función flecha dentro de un loop 'printNumbers
     -- numbers = [10,20,30,40,50,60,70,80,90,100] */
+
+console.log('►►► Array utilizada => [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]');
 
 const numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
@@ -69,27 +72,129 @@ console.log('►► Ejercicio 5');
     -- que imprima el mensaje 'Acaban de pasar 3 segundos' en consola */
 
 setTimeout(() => {
-  console.log('Buah! 5 segundos... Locura.');
+  console.log('AF => Nivel 3 => Ejercicio 5: Buah! 5 segundos... Locura.');
 }, 5000);
 setTimeout(() => {
-  console.log('Ahora acaban de pasar 3 segundos');
+  console.log('AF => Nivel 3 => Ejercicio 5: Ahora acaban de pasar 3 segundos');
 }, 3000);
 setTimeout(() => {
-  console.log('Acaba de pasar 1 segundos');
+  console.log('AF => Nivel 3 => Ejercicio 5: Acaba de pasar 1 segundos');
 }, 1000);
 
 console.log('----------------------------------------------');
 console.log('### Operador Ternario');
 console.log('----------------------------------------------');
 console.log('► Nivel 1');
-console.log('►► Ejercicio 1');
+console.log('►► Ejercicio 1 [Introduce la edad en el DOM]');
 
 /*  -- Ejercicio 1:
-    -- Operador ternario básico */
+    -- Operador ternario básico 
+    -- ¿tienes edad para poder conducir? */
 
 const potConduir = () => {
-  let edad = prompt('¿Cuantos años tienes?');
-  const mensaje = edad >= 18 ? 'Genial!! Puedes conducir!' : 'Lo siento! NO Puedes conducir!';
+  const edad = parseInt(prompt('¿Cuantos años tienes?'));
+  let mensaje = edad >= 18 ? 'Genial!! Puedes conducir!' : 'Lo siento! NO Puedes conducir!';
   console.log(mensaje); // Mostrar en la consola
   document.querySelector('#aptoParaConducir').innerHTML = mensaje; // Mostrar en el HTML
 };
+
+// -------------------------------------------------------------------
+
+console.log('►► Ejercicio 2 [Introduce los números en el DOM]');
+
+/*  -- Ejercicio 2:
+    -- Operador ternario de compración  
+    -- compara 2 números para ver cuál es más grande */
+
+const compararNum = () => {
+  let num1 = parseInt(prompt('Escribe el número 1'));
+  let num2 = parseInt(prompt('Escribe el número 2'));
+
+  let mensaje = num1 > num2 ? 'El num1 es más grande que el num2' : 'El num2 es más grande o igual que el num1';
+  console.log(mensaje); // Mostrar en la consola
+  document.querySelector('#resultadoCompararNum').innerHTML = mensaje; // Mostrar en el HTML
+};
+
+// -------------------------------------------------------------------
+
+console.log('----------------------------------------------');
+console.log('► Nivel 2');
+console.log('►► Ejercicio 3 [Introduce los valores a determinar en el DOM]');
+
+/*  -- Ejercicio 3:
+    -- Operador ternario enlazado  */
+
+const determinarNum = () => {
+  const userNum = prompt('Escriba el número a determinar');
+
+  let userNumDeterminado =
+    userNum == 0 ? 'El valor es 0' : userNum > 0 ? 'El valor es positivo' : 'El valor es negativo';
+  console.log(userNumDeterminado); // Mostrar en la consola
+  document.querySelector('#determinarNum').innerHTML = userNumDeterminado; // Mostrar en el HTML
+};
+
+// -------------------------------------------------------------------
+
+console.log('►► Ejercicio 3.b [Introduce 3 valores en el DOM]');
+
+/*  -- Ejercicio 3.b:
+    -- Operador ternario enlazado
+    -- Valor máximo de 3 parámetros  */
+
+const maxNum = () => {
+  let num1 = parseInt(prompt('Introduce le primer número'));
+  let num2 = parseInt(prompt('Introduce le segundo número'));
+  let num3 = parseInt(prompt('Introduce le tercer número'));
+
+  let valorMax =
+    num1 > num2 && num1 > num3
+      ? 'El valor máximo es el num1'
+      : num2 > num1 && num2 > num3
+      ? 'El valor máximo es el num2'
+      : 'El valor máximo es el num3';
+
+  console.log(valorMax); // Mostrar en la consola
+  document.querySelector('#maxNum').innerHTML = valorMax; // Mostrar en el HTML
+};
+
+// -------------------------------------------------------------------
+
+console.log('----------------------------------------------');
+console.log('► Nivel 3');
+console.log('►► Ejercicio 4');
+
+/*  -- Ejercicio 4 :
+    -- parOImparArray = [1,2,3,4,5,6,7,8,9,10] */
+
+console.log('►►► Array utilizada => => [1,2,3,4,5,6,7,8,9,10]');
+
+const parOImparArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const parOImpar = (parOImparArray) => {
+  for (let i = 0; i < parOImparArray.length; i++) {
+    let mensaje = parOImparArray[i] % 2 ? `${[i]} es par` : `${[i]} es inpar`;
+    console.log(mensaje);
+  }
+};
+parOImpar(parOImparArray);
+
+console.log('----------------------------------------------');
+console.log('### Callbacks');
+console.log('----------------------------------------------');
+console.log('► Nivel 1');
+console.log('►► Ejercicio 1 [Introduce la edad en el DOM]');
+
+/*  -- Ejercicio 1:
+    -- Callback básico 
+    -- función: procesar invoca función de callback */
+
+/* function processar(num, callback) {
+  // hacemo algo ...
+  num = 10;
+  // despues de hacer algo ...
+  callback(num);
+}
+
+processar(nums, function (num) {
+  console.log(`He añadido el ${num} como parámetro`);
+}); */
